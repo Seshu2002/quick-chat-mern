7b7fefd7f4832043c22dffdea021ebeb2131c214
Seshu2002 <sheshusaichinni@gmail.com>
2024-07-29 12:07:26 +0530

    Search User updated

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -3,15 +3,19 @@ import { IoMdChatboxes } from 'react-icons/io';
 import { FaUserPlus } from "react-icons/fa";
 import { NavLink } from 'react-router-dom';
 import { BiLogOut } from "react-icons/bi";
-import Avatar from "./Avatar";
+import { FiArrowUpLeft } from "react-icons/fi";
 import { useSelector } from 'react-redux';
+
+import Avatar from "./Avatar";
 import EditUserDetails from './EditUserDetails';
-import Divider from './Divider';
+import SearchUser from "./SearchUser";
+
 
 const Sidebar = () => {
     const user = useSelector(state => state?.user);
     const [editUserOpen, setEditUserOpen] = useState(false);
-    const [allUser,setAllUser] = useState([])
+    const [allUser, setAllUser] = useState([])
+    const [openSeacrhUser, setOpenSearchUser] = useState(false);
 
     return (
         <div className='w-full h-full grid grid-cols-[48px,1fr] bg-white'>
@@ -20,7 +24,7 @@ const Sidebar = () => {
                     <NavLink title='Chat' className={({ isActive }) => `w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded ${isActive && "bg-slate-200"}`}>
                         <IoMdChatboxes size={20} />
                     </NavLink>
-                    <div title='Add Friend' className='w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded'>
+                    <div title='Add Friend' onClick={()=>setOpenSearchUser(true)} className='w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded'>
                         <FaUserPlus size={20} />
                     </div>
                 </div>
@@ -38,17 +42,18 @@ const Sidebar = () => {
 
             <div className='w-full'>
                 <div className='h-16 flex items-center'>
-                <h2 className='text-xl font-bold p-4 text-slate-800'>Message</h2>
+                    <h2 className='text-xl font-bold p-4 text-slate-800'>Message</h2>
                 </div>
                 <div className='bg-slate-200 p-[0.5px]'></div>
 
                 <div className='h-[calc(100vh-65px)] overflow-x-hidden overflow-y-auto scrollbar'>
                     {
                         allUser.length === 0 && (
-                            <div>
-                                <div>
-                                    <p className='text-lg text-center text-slate-600'>Explore users to start a conversation with.</p>
+                            <div className='mt-10'>
+                                <div className='flex justify-center items-center my-4 text-slate-500'>
+                                    <FiArrowUpLeft size={50} />
                                 </div>
+                                <p className='text-lg text-center text-slate-600'>Explore users to start a conversation with.</p>
                             </div>
                         )
                     }
@@ -59,6 +64,13 @@ const Sidebar = () => {
             {editUserOpen && (
                 <EditUserDetails onClose={() => setEditUserOpen(false)} user={user} />
             )}
+
+            {/* Search User */}
+            {
+                openSeacrhUser && (
+                    <SearchUser onClose={() => setOpenSearchUser(false)}/>
+                )
+            }
         </div>
     );
 };
