b940ff4698290dca0fca101305461a0f95e6667d
Seshu Sai <sheshusaichinni@gmail.com>
2024-08-03 17:01:12 +0530

    Edit User details working

--- client/src/components/Avatar.js
+++ client/src/components/Avatar.js
@@ -1,64 +1,66 @@
 import React from 'react';
-import {PiUserCircle} from 'react-icons/pi'
+import { PiUserCircle } from 'react-icons/pi';
 import { useSelector } from 'react-redux';
 
 const Avatar = ({ userId, name, imageUrl, width, height }) => {
-    const onlineUser = useSelector(state => state?.user?.onlineUser)
-    let avatarName = ""
-    if(name){
-        const splitName = name?.split(" ");
-        if(splitName.length > 1){
-            avatarName = splitName[0][0]+splitName[1][0];
-        } else{
+    const onlineUser = useSelector(state => state?.user?.onlineUser);
+    let avatarName = "";
+    
+    if (name) {
+        const splitName = name.split(" ");
+        if (splitName.length > 1) {
+            avatarName = splitName[0][0] + splitName[1][0];
+        } else {
             avatarName = splitName[0][0];
         }
     }
-    
-        const bgColor = [
-            'bg-slate-200',
-            'bg-teal-200',
-            'bg-red-200',
-            'bg-green-200',
-            'bg-yellow-200',
-            'bg-blue-200',
-            'bg-violet-200',
-            'bg-pink-200',
-            'bg-gray-200',
-            'bg-cyan-200',
-            'bg-sky-200',
-        ]
 
-        const randomNumber = Math.floor(Math.random() * bgColor.length)
+    const bgColor = [
+        'bg-slate-200',
+        'bg-teal-200',
+        'bg-red-200',
+        'bg-green-200',
+        'bg-yellow-200',
+        'bg-blue-200',
+        'bg-violet-200',
+        'bg-pink-200',
+        'bg-gray-200',
+        'bg-cyan-200',
+        'bg-sky-200',
+    ];
 
-        const isOnline = onlineUser.includes(userId)
+    const randomNumber = Math.floor(Math.random() * bgColor.length);
+    const isOnline = onlineUser.includes(userId);
 
     return (
-        <div className={`text-slate-800 rounded-full font-bold relative`} style={{width : width+"px", height : height+"px" }}>
-            {
-                imageUrl ? (
-                    <img src={imageUrl} width={width} height={height} alt={name} className='overflow-hidden rounded-full'/>
+        <div
+            className={`relative rounded-full overflow-hidden ${isOnline ? 'border-2 border-green-500' : ''}`}
+            style={{ width: width + 'px', height: height + 'px' }}
+        >
+            {imageUrl ? (
+                <img
+                    src={imageUrl}
+                    alt={name}
+                    className="w-full h-full object-cover rounded-full"
+                />
+            ) : (
+                name ? (
+                    <div
+                        className={`w-full h-full flex items-center justify-center text-lg font-bold ${bgColor[randomNumber]}`}
+                    >
+                        {avatarName}
+                    </div>
                 ) : (
-                    name ? (
-                        <div style={{width : width+"px", height : height+"px" }} className={`overflow-hidden rounded-full flex justify-center items-center text-lg ${bgColor[randomNumber]}`}>
-                            {avatarName}
-                        </div>
-                    ) : (
-                        <div>
-                            <PiUserCircle
-                                size={width}
-                            />
-                        </div>
-                    )
+                    <div className="w-full h-full flex items-center justify-center">
+                        <PiUserCircle size={width} />
+                    </div>
                 )
-            }
-             {
-                isOnline && (
-                    <div className='bg-green-500 p-1 absolute bottom-2 -right-1 z-10 rounded'></div>
-                )
-             }
+            )}
+            {isOnline && (
+                <div className="absolute bottom-1 right-1 w-3 h-3 bg-green-500 rounded-full border border-white"></div>
+            )}
         </div>
-    )
-
-}
+    );
+};
 
 export default Avatar;
